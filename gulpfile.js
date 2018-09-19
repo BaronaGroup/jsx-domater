e49831deda9eaa69a7afa93f6748becd97455be5
Tapani Haka <tapani.haka@barona.fi>
2018-09-19 18:27:45 +0300

    Prepare for distribution

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,25 @@ gulp.task('clean-build', function () {
     .pipe(clean())
 })
 
-gulp.task('clean', ['clean-build'])
+gulp.task('clean-dist', function () {
+  return gulp.src('dist', {read: false})
+    .pipe(clean())
+})
+
+gulp.task('clean', ['clean-build', 'clean-dist'])
 
 gulp.task('ts', function () {
   return tsProject.src()
     .pipe(tsProject())
     .pipe(gulp.dest('build'))
+})
+
+gulp.task('dist', ['clean', 'ts'], function() {
+  // Typescript is supposed to do this for us, but it creates js failes instead of json files and still attempts
+  // to require the json files, which of course fails. This is a quick and easy workaround.
+  return gulp
+    .src('build/src/**/*')
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('build', ['ts'])
